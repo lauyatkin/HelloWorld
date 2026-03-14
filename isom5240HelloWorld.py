797caaa57587c42391ef1ee835797c06d2c42686
lauyatkin <50213891+lauyatkin@users.noreply.github.com>
2026-03-14 15:36:04 +0800

    Update isom5240HelloWorld.py

--- isom5240HelloWorld.py
+++ isom5240HelloWorld.py
@@ -25,4 +25,5 @@ predictions = predictions.cpu().detach().numpy()
 # Get the index of the largest output value
 max_index = np.argmax(predictions)
 
-print(f"The label is {label} and the predicted label is {max_index}")
+# print(f"The label is {label} and the predicted label is {max_index}")
+st.write(f"The label is {label} and the predicted label is {max_index}")
