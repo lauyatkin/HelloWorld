fee4ed5c97af40a9d6b7735cb440415bff8b1dd2
lauyatkin <50213891+lauyatkin@users.noreply.github.com>
2026-03-18 17:57:19 +0800

    Update isom5240HelloWorld.py

--- isom5240HelloWorld.py
+++ isom5240HelloWorld.py
@@ -1,5 +1,25 @@
 import streamlit as st
-
 from transformers import pipeline
-pipe = pipeline("text-generation", model="facebook/xglm-564M")
 
+def main():
+    sentiment_pipeline = pipeline(model="ivanwonghs/trial_1")
+    replyMsg_pipeline = pipeline("text-generation", model="facebook/xglm-564M")
+
+    st.title("Muti-language Reply Message Generator For Negative Comment")
+    st.write("Enter a comment to generate reply message in the corresponding language: ")
+
+    user_input = st.text_input("")
+    if user_input:
+        sentiment_result = sentiment_pipeline(user_input)
+        sentiment = sentiment_result[0]["label"]
+        confidence = sentiment_result[0]["score"]
+
+        st.write(f"Sentiment: {sentiment}")
+        st.write(f"Confidence: {confidence:.2f}")
+
+        replyMsg_result = replyMsg_pipeline(f"Generate a polite reply to apologize in corresponding language for below message: '{user_input}'")
+        st.write(replyMsg_result)
+        st.write(f"Reply Message: {confidence:.2f}")
+
+if name == "main":
+    main()
